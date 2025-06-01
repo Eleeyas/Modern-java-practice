2da85e936aef552b9c71662b736b8fcb8d0075a4
Eleeyas <ticktock_102@outlook.com>
2025-06-01 17:32:18 +0700

    init

---
+++ src/main/java/com/example/modernjavapractice/ModernJavaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.modernjavapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernJavaPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModernJavaPracticeApplication.class, args);
+    }
+
+}
